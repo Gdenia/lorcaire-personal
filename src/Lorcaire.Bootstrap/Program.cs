356a1e85eda2b364639d54cc66f076d72b4238bc
Denia <gdenia1450@gmail.com>
2026-08-07 02:06:25 +0200

    Create Bootstrap startup

---
+++ src/Lorcaire.Bootstrap/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("====================================");
+Console.WriteLine("      Lorcaire Personal Edition");
+Console.WriteLine("====================================");
+Console.WriteLine();
+Console.WriteLine("Inicializando Runtime...");
+Console.WriteLine("Runtime listo.");
